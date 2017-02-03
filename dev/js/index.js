e60fd812f38cb186d19422665f2b419d52dac268
Jcook894 <jcook894@icloud.com>
2017-02-03 00:14:05 -0500

    feat: added user reducer and giving it users.

--- dev/js/index.js
+++ dev/js/index.js
@@ -1,23 +1,10 @@
 import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from "react-dom";
-import {Provider} from 'react-redux';
-import {createStore, applyMiddleware} from 'redux';
-import thunk from 'redux-thunk';
-import promise from 'redux-promise';
-import createLogger from 'redux-logger';
-import allReducers from './reducers';
-import App from './components/App';
 
-const logger = createLogger();
-const store = createStore(
-    allReducers,
-    applyMiddleware(thunk, promise, logger)
-);
+const store = createStore();
 
 ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>,
+    <h1>Hey now</h1>,
     document.getElementById('root')
 );
